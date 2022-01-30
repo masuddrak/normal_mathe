afe552f2ca337fb2c8e005794022f2820febe939
masudrana <masudranaacb@gmail.com>
2022-01-31 03:10:05 +0600

    switch case using a letter

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 // vaoil & consonent
 
-let letter='A';
+let letter='c';
     letter=letter.toLowerCase();
-if(letter=='a' || letter=='e' || letter=='i'|| letter=='o'|| letter=='u'){
-    console.log('thats is vaoil');
-}else{
-    console.log('is na a vaaoil is consonent');
+switch(letter){
+    case 'a':
+        console.log('that is a');
+        break;
+    case 'b':
+        console.log("thats is a b");
+        break;
+    default :
+    console.log('is not latter');
 }
