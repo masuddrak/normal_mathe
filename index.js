928dab3547517f6003c1c5007049614a0fafa6f5
masudrana <masudranaacb@gmail.com>
2022-01-31 02:59:00 +0600

    vaol & consonents difin

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-// slice mathod using
-let cuntry='bangladesh';
-let countryAvriviatin=cuntry.slice(1,5);
-console.log(countryAvriviatin);
+// vaoil & consonent
+
+let letter='A';
+    letter=letter.toLowerCase();
+if(letter=='a' || letter=='e' || letter=='i'|| letter=='o'|| letter=='u'){
+    console.log('thats is vaoil');
+}else{
+    console.log('is na a vaaoil is consonent');
+}
