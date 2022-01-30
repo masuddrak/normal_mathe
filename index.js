c213fa430c5a76f72706546aeb5a0836d1824e0d
masudrana <masudranaacb@gmail.com>
2022-01-31 02:09:28 +0600

    add slice mathod usin

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-var fristName='masud';
-var lastName=' rana';
-var fullName=fristName.concat(lastName);
-
-
-console.log(fullName);
+// slice mathod using
+let cuntry='bangladesh';
+let countryAvriviatin=cuntry.slice(1,5);
+console.log(countryAvriviatin);
